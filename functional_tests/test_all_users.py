09175ea2ef5afc5e521ea5ce103140b57af34add
Oscar <OscarBellon96@gmail.com>
2016-02-09 19:07:53 +0100

    Added translations

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from django.core.urlresolvers import reverse
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase  
- 
- 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.utils.translation import activate
+
 class HomeNewVisitorTest(StaticLiveServerTestCase):
- 
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
- 
+        activate('en')
+
     def tearDown(self):
         self.browser.quit()
- 
+
     def get_full_url(self, namespace):
         return self.live_server_url + reverse(namespace)
- 
+
     def test_home_title(self):
         self.browser.get(self.get_full_url("home"))
         self.assertIn("TaskBuster", self.browser.title)
- 
+
     def test_h1_css(self):
         self.browser.get(self.get_full_url("home"))
         h1 = self.browser.find_element_by_tag_name("h1")
-        self.assertEqual(h1.value_of_css_property("color"), 
+        self.assertEqual(h1.value_of_css_property("color"),
                          "rgba(200, 50, 255, 1)")
 
     def test_home_files(self):
@@ -31,3 +32,11 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn("Not Found", self.browser.title)
         self.browser.get(self.live_server_url + "/humans.txt")
         self.assertNotIn("Not Found", self.browser.title)
+
+    def test_internationalization(self):
+    for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
+                          ('es', 'Bienvenidos a TaskBuster!')]:
+        activate(lang)
+        self.browser.get(self.get_full_url("home"))
+        h1 = self.browser.find_element_by_tag_name("h1")
+        self.assertEqual(h1.text, h1_text)
--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -21,14 +21,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from django.core.exceptions import ImproperlyConfigured
- 
+
+from django.utils.translation import ugettext_lazy as _
+
 def get_env_variable(var_name):
     try:
         return os.environ[var_name]
     except KeyError:
         error_msg = "Set the %s environment variable" % var_name
         raise ImproperlyConfigured(error_msg)
- 
+
 SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -48,6 +50,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -70,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -91,6 +95,15 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('es', _('Spanish')),
+)
 
 LANGUAGE_CODE = 'en-us'
 
